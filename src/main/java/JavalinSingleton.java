d3a1149bc297c270f9884f8664a905e865f86771
JosephReed75 <josephreed13@yahoo.com>
2023-03-02 23:42:59 +0000

    Completed Creating EndPoints

--- src/main/java/JavalinSingleton.java
+++ src/main/java/JavalinSingleton.java
@@ -18,6 +18,11 @@ public class JavalinSingleton {
         Javalin app = Javalin.create();
         
         //write endpoint here
+        app.get("/hello", ctx -> {
+            ctx.result("Hello World");
+        });
+
+
 
         return app;
     }
